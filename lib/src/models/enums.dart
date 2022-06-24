83fa8312d1d3f481215998a4c88fb740aa97d373
Jay-flow <kimpetro153@gmail.com>
2022-06-24 14:49:56 +0900

    Fix the image loading fot the pub (#3)

--- lib/src/models/enums.dart
+++ lib/src/models/enums.dart
@@ -17,19 +17,19 @@ enum CalendarSelectionMode {
   /// selecting a new date will remove the selection for previous date and
   /// updates selection to the new selected date.
   ///
-  /// ![single_demo](https://github.com/dooboolab/flutter_calendar_widget/blob/main/doc/single_demo.gif)
+  /// ![single_demo](https://raw.githubusercontent.com/dooboolab/flutter_calendar_widget/main/doc/single_demo.gif)
   single,
 
   /// [CalendarSelectionMode.multiple], Allows multiple date selection,
   /// selecting a new date will not remove the selection for previous dates,
   /// allows to select as many dates as possible.
   ///
-  /// ![multiple_demo](https://github.com/dooboolab/flutter_calendar_widget/blob/main/doc/multiple_demo.gif)
+  /// ![multiple_demo](https://raw.githubusercontent.com/dooboolab/flutter_calendar_widget/main/doc/multiple_demo.gif)
   multiple,
 
   /// [CalendarSelectionMode.range], Allows to select a single range of
   /// dates.
   ///
-  /// ![range_demo](https://github.com/dooboolab/flutter_calendar_widget/blob/main/doc/range_demo.gif)
+  /// ![range_demo](https://raw.githubusercontent.com/dooboolab/flutter_calendar_widget/main/doc/range_demo.gif)
   range,
 }
